c8f824e26300f9edcb89d5bee66ef876981bc973
Simon Wicky <simon.wicky@epfl.ch>
2020-09-17 11:18:43 +0200

    added id to received message

--- template_java/src/main/java/cs451/NetworkManager.java
+++ template_java/src/main/java/cs451/NetworkManager.java
@@ -12,21 +12,27 @@ import java.util.List;
 
 public class NetworkManager {
 
-    private int id;
-    private HashMap<Integer, Host> hosts;
+    private HashMap<Integer, Host> idToHosts;
+    private HashMap<HostInfo, Integer> hostToId;
     private DatagramSocket socket;
 
     public NetworkManager(List<Host> hosts, int id) {
-        this.hosts = new HashMap<>();
-        for (Host host : hosts){
-            this.hosts.put(host.getId(), host);
-        }
+        this.idToHosts = new HashMap<>();
+        this.hostToId = new HashMap<>();
+
         try {
-            socket = new DatagramSocket(this.hosts.get(id).getPort(), InetAddress.getByName(this.hosts.get(id).getIp()));
+            //mapping setup, for fast sending and receiving
+            for (Host host : hosts){
+                this.idToHosts.put(host.getId(), host);
+                HostInfo info = new HostInfo(InetAddress.getByName(host.getIp()),host.getPort());
+                this.hostToId.put(info, host.getId());
+            }
+
+            //socket creation
+            socket = new DatagramSocket(this.idToHosts.get(id).getPort(), InetAddress.getByName(this.idToHosts.get(id).getIp()));
         } catch (SocketException | UnknownHostException e) {
             System.out.println("Socket could not be created: " + e.getMessage());
         }
-        this.id = id;
     }
 
     public void sendTo(int neighbourIndex, long msg) throws IOException {
@@ -34,20 +40,62 @@ public class NetworkManager {
         byte[] buf = ByteBuffer.allocate(8).putLong(msg).array();
 
         //datagram setup
-        DatagramPacket packet = new DatagramPacket(buf, 8, InetAddress.getByName(hosts.get(neighbourIndex).getIp()), hosts.get(neighbourIndex).getPort());
+        DatagramPacket packet = new DatagramPacket(buf, 8, InetAddress.getByName(idToHosts.get(neighbourIndex).getIp()), idToHosts.get(neighbourIndex).getPort());
 
         //send datagram
         socket.send(packet);
     }
 
-    public long receive() throws IOException{
+    //return an array of two longs, id of sender and message
+    public long[] receive() throws IOException{
         byte[] buf = new byte[8];
         DatagramPacket packet = new DatagramPacket(buf,8);
         socket.receive(packet);
-        return ByteBuffer.wrap(buf).getLong();
+        System.out.println(hostToId);
+        int id = hostToId.get(new HostInfo(packet.getAddress(), packet.getPort()));
+        return new long[] {id, ByteBuffer.wrap(buf).getLong()};
     }
 
     public void closeSocket() {
         socket.close();
+    }
+
+    //helper class for a fast lookup Host to Id
+    private static class HostInfo {
+        private InetAddress ip;
+        private int port;
+
+        public HostInfo(InetAddress ip, int port){
+            this.ip = ip;
+            this.port = port;
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            HostInfo other = (HostInfo) obj;
+            if (ip == null) {
+                if (other.ip != null)
+                    return false;
+            } else if (!ip.equals(other.ip))
+                return false;
+            if (port != other.port)
+                return false;
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((ip == null) ? 0 : ip.hashCode());
+            result = prime * result + port;
+            return result;
+        }
     }
 }
