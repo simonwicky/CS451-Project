c0c986859799a7eba490e296a9884ca786b28275
Simon Wicky <simon.wicky@epfl.ch>
2020-10-19 11:22:10 +0200

    fifo implementation

--- template_java/src/main/java/cs451/FIFOBroadcast.java
+++ template_java/src/main/java/cs451/FIFOBroadcast.java
@@ -5,8 +5,16 @@ import java.util.List;
 
 public class FIFOBroadcast extends URBBroadcast {
 
+    private long[] vc;
+    private List<Broadcaster.Message> pending;
+
     public FIFOBroadcast(List<Host> hosts, int id, long nb_msg) {
         super(hosts, id, nb_msg);
+        this.vc = new long[hosts.size() + 1];
+        for (int i = 0; i < vc.length; i++) {
+            vc[i] = 1;
+        }
+        this.pending = new ArrayList<>();
     }
 
     protected void broadcast(byte[] msg) {
@@ -15,9 +23,38 @@ public class FIFOBroadcast extends URBBroadcast {
 
     protected ArrayList<Broadcaster.Message> handleMsg(byte[] msg, int from) {
         ArrayList<Broadcaster.Message> message = super.handleMsg(msg, from);
+        ArrayList<Broadcaster.Message> deliveredMessage = new ArrayList<>();
         if (message != null) {
-            // do fifo magic
-            return message;
+            boolean newMsg = false;
+            // check for delivered message
+            for (Broadcaster.Message m : message) {
+                if (vc[m.getId()] == m.getMsgId()) {
+                    deliveredMessage.add(m);
+                    newMsg = true;
+                    vc[m.getId()]++;
+                } else {
+                    pending.add(m);
+                }
+            }
+
+            // check in pending
+            // if no message is delivered from the fresh ones, no need to check, no changes
+            // if a message from pending is delivered, mabye a previous one can be too,
+            // hence redo the loop
+            while (newMsg) {
+                newMsg = false;
+                for (Broadcaster.Message m : new ArrayList<>(pending)) {
+                    if (vc[m.getId()] == m.getMsgId()) {
+                        deliveredMessage.add(m);
+                        newMsg = true;
+                        pending.remove(m);
+                        vc[m.getId()]++;
+                    }
+                }
+
+            }
+
+            return deliveredMessage;
         } else {
             return null;
         }
